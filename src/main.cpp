3edc34b2e94ff243379f263dae57da5bfb03ca0c
luswdev <610721230@gms.ndhu.edu.tw>
2023-03-10 11:13:59 +0800

    [M] move test case to test.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,5 @@
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions)
-{
-    EXPECT_STRNE("hello", "world");
-    EXPECT_EQ(7 * 6, 42);
-}
-
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
