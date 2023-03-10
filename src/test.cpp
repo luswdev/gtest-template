a175ca7d829078d855102735018caa958457d8c2
luswdev <610721230@gms.ndhu.edu.tw>
2023-03-10 11:21:42 +0800

    [A] test value

--- src/test.cpp
+++ src/test.cpp
@@ -5,3 +5,26 @@ TEST(template, TestBool)
     EXPECT_TRUE( 1 == 1);
     EXPECT_FALSE( 1 == 0);
 }
+
+TEST(template, TestValue)
+{
+    /* L == R */
+    EXPECT_EQ( 1, 1);
+
+    /* L != R */
+    EXPECT_NE(1, 0);
+
+    /* L < R */
+    EXPECT_LT(1, 2);
+
+    /* L <= R */
+    EXPECT_LE(1, 1);
+    EXPECT_LE(1, 2);
+
+    /* L > R */
+    EXPECT_GT(2, 1);
+
+    /* L <= R */
+    EXPECT_GE(2, 2);
+    EXPECT_GE(2, 1);
+}
